d242a750f91a3bf73dc0d5086940f84251803946
mosimchah <mosimchah@gmail.com>
2026-05-15 00:39:51 -0400

    ThemePicker: Add custom color tab
    Change-Id: I82bcc7e8de7e6803bf4818d999767edfe8974adc

--- src/com/android/customization/picker/color/data/repository/ColorPickerRepositoryImpl2.kt
+++ src/com/android/customization/picker/color/data/repository/ColorPickerRepositoryImpl2.kt
@@ -89,6 +89,11 @@ constructor(
                                 val presetColorOptions: MutableList<ColorOption> = mutableListOf()
                                 val aicpColorOptions: MutableList<ColorOption> =
                                     mutableListOf()
+                                val customColorOptions: MutableList<ColorOption> =
+                                    mutableListOf()
+                                options?.firstOrNull()?.let {
+                                    customColorOptions.add(it)
+                                }
                                 options?.forEach { option ->
                                     when ((option as ColorOptionImpl).type) {
                                         ColorType.WALLPAPER_COLOR ->
@@ -96,6 +101,8 @@ constructor(
                                         ColorType.PRESET_COLOR -> presetColorOptions.add(option)
                                         ColorType.AICP_COLOR ->
                                             aicpColorOptions.add(option)
+                                        ColorType.CUSTOM_COLOR ->
+                                            customColorOptions.add(option)
                                     }
                                 }
                                 continuation.resumeWith(
@@ -103,6 +110,7 @@ constructor(
                                         mapOf(
                                             ColorType.WALLPAPER_COLOR to wallpaperColorOptions,
                                             ColorType.AICP_COLOR to aicpColorOptions,
+                                            ColorType.CUSTOM_COLOR to customColorOptions,
                                             ColorType.PRESET_COLOR to presetColorOptions,
                                         )
                                     )
