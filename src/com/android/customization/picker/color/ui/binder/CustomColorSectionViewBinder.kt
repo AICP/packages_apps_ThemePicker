b78f44148e6b0080a4ef97a2d0d0ec04a4598b14
mosimchah <mosimchah@gmail.com>
2026-05-15 00:39:51 -0400

    ThemePicker: Add custom color tab
    Change-Id: I82bcc7e8de7e6803bf4818d999767edfe8974adc

---
+++ src/com/android/customization/picker/color/ui/binder/CustomColorSectionViewBinder.kt
@@ -0,0 +1,22 @@
+package com.android.customization.picker.color.ui.binder
+
+import android.view.View
+import com.android.customization.picker.color.ui.viewmodel.ColorPickerViewModel
+import net.margaritov.preference.colorpicker.ColorPickerDialog
+
+object CustomColorSectionViewBinder {
+    fun bind(
+        view: View,
+        viewModel: ColorPickerViewModel,
+    ) {
+        view.setOnClickListener {
+            val currentColor = view.context.getColor(android.R.color.system_accent1_500)
+            val dialog = ColorPickerDialog(view.context, currentColor)
+            dialog.setAlphaSliderVisible(false)
+            dialog.setOnColorChangedListener { color ->
+                viewModel.interactor.applyCustomColor(view.context, color)
+            }
+            dialog.show()
+        }
+    }
+}
--- src/com/android/customization/picker/color/ui/viewmodel/ColorPickerViewModel.kt
+++ src/com/android/customization/picker/color/ui/viewmodel/ColorPickerViewModel.kt
@@ -29,6 +29,7 @@ import com.android.wallpaper.picker.common.text.ui.viewmodel.Text
 import com.android.wallpaper.picker.option.ui.viewmodel.OptionItemViewModel
 import kotlin.math.max
 import kotlin.math.min
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -38,11 +39,10 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 /** Models UI state for a color picker experience. */
-class ColorPickerViewModel
-private constructor(
-    context: Context,
-    private val interactor: ColorPickerInteractor,
-    private val logger: ThemesUserEventLogger,
+class ColorPickerViewModel @Inject constructor(
+    private val context: Context,
+    internal val interactor: ColorPickerInteractor,
+    @BackgroundDispatcher private val bgDispatcher: CoroutineDispatcher,
 ) : ViewModel() {
 
     private val selectedColorTypeTabId = MutableStateFlow<ColorType?>(null)
@@ -200,7 +200,7 @@ private constructor(
 
     class Factory(
         private val context: Context,
-        private val interactor: ColorPickerInteractor,
+        internal val interactor: ColorPickerInteractor,
         private val logger: ThemesUserEventLogger,
     ) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
