cbcbab5a7b231ed7fc7356780084dac490a540ee
mosimchah <mosimchah@gmail.com>
2026-05-15 00:39:51 -0400

    ThemePicker: Add custom color tab
    Change-Id: I82bcc7e8de7e6803bf4818d999767edfe8974adc

--- src/com/android/customization/picker/color/data/repository/ColorPickerRepositoryImpl.kt
+++ src/com/android/customization/picker/color/data/repository/ColorPickerRepositoryImpl.kt
@@ -67,6 +67,7 @@ constructor(
                             mapOf(
                                 ColorType.WALLPAPER_COLOR to listOf(),
                                 ColorType.AICP_COLOR to listOf(),
+                                ColorType.CUSTOM_COLOR to listOf(),
                                 ColorType.PRESET_COLOR to listOf(),
                             )
                         )
@@ -84,6 +85,8 @@ constructor(
                             val presetColorOptions: MutableList<ColorOptionModel> = mutableListOf()
                             val aicpColorOptions: MutableList<ColorOptionModel> =
                                 mutableListOf()
+                            val customColorOptions: MutableList<ColorOptionModel> =
+                                mutableListOf()
                             options?.forEach { option ->
                                 when ((option as ColorOptionImpl).type) {
                                     ColorType.WALLPAPER_COLOR ->
@@ -92,6 +95,8 @@ constructor(
                                         presetColorOptions.add(option.toModel())
                                     ColorType.AICP_COLOR ->
                                         aicpColorOptions.add(option.toModel())
+                                    ColorType.CUSTOM_COLOR ->
+                                        customColorOptions.add(option.toModel())
                                 }
                             }
                             continuation.resumeWith(
@@ -99,6 +104,7 @@ constructor(
                                     mapOf(
                                         ColorType.WALLPAPER_COLOR to wallpaperColorOptions,
                                         ColorType.AICP_COLOR to aicpColorOptions,
+                                        ColorType.CUSTOM_COLOR to customColorOptions,
                                         ColorType.PRESET_COLOR to presetColorOptions,
                                     )
                                 )
